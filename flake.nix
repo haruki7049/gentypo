472dd3fa894c4789487309b5565b973e4ce51fd4
haruki7049 <tontonkirikiri@gmail.com>
2024-12-17 11:33:41 +0900

    chore(flakes): Edit inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
   outputs =
