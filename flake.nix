8c1c96f6e4a33fc0b2a630d10d613edbf9f989a0
haruki7049 <tontonkirikiri@gmail.com>
2025-01-02 04:49:49 +0900

    Edit flakes

--- flake.nix
+++ flake.nix
@@ -1,10 +1,22 @@
 {
+  description = "A simple libc, made by Rust-lang";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    crane.url = "github:ipetkov/crane";
+    flake-compat.url = "github:edolstra/flake-compat";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -13,12 +25,80 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
 
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.nil
-          ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          overlays = [ inputs.rust-overlay.overlays.default ];
+          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rust;
+          src = lib.cleanSource ./.;
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src;
+          };
+          ferlibc = craneLib.buildPackage {
+            inherit src cargoArtifacts;
+            strictDeps = true;
+            doCheck = true;
+          };
+          cargo-clippy = craneLib.cargoClippy {
+            inherit src cargoArtifacts;
+            cargoClippyExtraArgs = "--verbose -- --deny warning";
+          };
+          cargo-doc = craneLib.cargoDoc {
+            inherit src cargoArtifacts;
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+          };
+
+          checks = {
+            inherit
+              ferlibc
+              cargo-clippy
+              cargo-doc
+              ;
+          };
+
+          packages = {
+            inherit
+              ferlibc
+              cargo-clippy
+              cargo-doc
+              ;
+            default = ferlibc;
+            doc = cargo-doc;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.rustfmt.enable = true;
+            #programs.taplo.enable = true; # BUG: taplo failed when I use it on aarch64-darwin
+            programs.actionlint.enable = true;
+            programs.mdformat.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              rust
+              pkgs.nil
+            ];
+
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]\$ "
+            '';
+          };
         };
-      };
     };
 }
